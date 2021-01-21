5fdf5d30874d9ec726b37bd452e20d3ccb14ec95
Victor Volovik <VictorVolovik@users.noreply.github.com>
2021-01-21 10:41:09 +0300

    Add simple structure

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,45 @@
-const Home = () => <>Our Home</>;
+import uuid from 'react-uuid';
+
+const fetchData = async () => {
+
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
+const Home = () => (
+  <article>
+    <h1>HealthCare.gov Articles</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Language</th>
+          <th>Article</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>en</td>
+          <td><a href="#" target="_blank">Article's title 1</a></td>
+          <td>2020-04-10 00:00:00 -0400</td>
+        </tr>
+        <tr>
+          <td>es</td>
+          <td><a href="#" target="_blank">Article's title 2</a></td>
+          <td>2020-04-10 00:00:00 -0400</td>
+        </tr>
+        <tr>
+          <td>en</td>
+          <td><a href="#" target="_blank">Article's title 3</a></td>
+          <td>2020-04-10 00:00:00 -0400</td>
+        </tr>
+      </tbody>
+    </table>
+  </article>
+);
 
 export default Home;
